fce66aa9422d110880d82d565a8ad380b260750b
Андрей <uytov1991@gmail.com>
2019-02-14 21:18:30 +0300

    Слайдер и функции

---
+++ src/js/slider.js
@@ -0,0 +1,6 @@
+const overlayImg = document.querySelector('.img-comp-overlay');
+const punktPos = document.querySelector('.position');
+const bar = document.querySelector('.bar');
+const scale = document.querySelector('.scale');
+
+const 
