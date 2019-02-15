a8b241d7e9430efdbcf74e96a7ff3585d2d94325
Andrey <uytov1991@gmail.com>
2019-02-15 15:43:31 +0400

    Верстка слайдера

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,42 @@
 const overlayImg = document.querySelector('.img-comp-overlay');
-const punktPos = document.querySelector('.position');
+const pos = document.querySelector('.position');
 const bar = document.querySelector('.bar');
 const scale = document.querySelector('.scale');
 
-const 
+
+
+const mouseDownOnPos = function (pos) {
+	pos.addEventListener('mousedown', function(evt) {
+		evt.preventDefult();
+
+		let startCoord = {
+			x: evt.ClientX
+		};
+
+		const mouseMoveOnPos = function (evtMove){
+			evtMove.preventDefult();
+			let shift = {
+				x: startCoord.x - evtMove.ClientX
+			};
+
+			startCoord = {
+				x: evtMove.ClientX
+			};
+			// Код для перемещения ползунка и бара
+			
+
+		};
+
+		// Не забудем снять обработчики
+		
+		const mouseUpOnDocument = function(mouseUpEvt){
+			mouseUpEvt.preventDefult();
+			document.removeEventListener('mousedown', mouseDownOnPos);
+			document.removeEventListener ('mousemove', mouseMoveOnPos);
+		};
+
+		document.addEventListener('mousemove', mouseMoveOnPos);
+		document.addEventListener('mouseup', mouseUpOnDocument);
+
+	})
+}
