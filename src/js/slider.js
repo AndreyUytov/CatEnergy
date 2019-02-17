48f3550a3ae55c6f1f4f57b829d6a57d3b1444c6
Андрей <uytov1991@gmail.com>
2019-02-17 17:31:24 +0300

    Доверстал до конца слайдер

--- src/js/slider.js
+++ src/js/slider.js
@@ -24,18 +24,33 @@ const mouseDownOnPos = function (pos) {
 				x: evtMove.clientX
 			};
 			// Код для перемещения ползунка и бара
-
-      if ((pos.offsetLeft - shift.x) < 0) {
-        pos.style.left = 0 + 'px';	 
-      } if ((pos.offsetLeft - shift.x) > 70) {
-        pos.style.left = 69 + 'px';
+      const posMover = function (positionLeft) {
+        var posPosition = null;
+      if ((positionLeft - shift.x) < 0) {
+        posPosition = 0;	 
+      } else if ((positionLeft - shift.x) > 70) {
+        posPosition = 69;
       } else {
-        pos.style.left = (pos.offsetLeft - shift.x) + 'px';
-      } 
+        posPosition = positionLeft - shift.x;
+      };
+      pos.style.left = posPosition + 'px';
+      return posPosition; 
+    };
+
+    ;
+
+    posMover(pos.offsetLeft);
 
       // Для бара
       
+     bar.style.width = pos.style.left;
+
+      // Для оверлея картинки
+
+      overlayImg.style.width = ((posMover(pos.offsetLeft)) * 3.8) + 'px';
 		};
+
+
 
 		// Не забудем снять обработчики
 		
@@ -52,3 +67,82 @@ const mouseDownOnPos = function (pos) {
 };
 
 mouseDownOnPos(pos);
+
+const clickAndTouchOnScale = function (scaleEvt) {
+    scaleEvt.preventDefault();
+    let coordinateClick = (scaleEvt.clientX - scale.getBoundingClientRect().left);
+    if ((coordinateClick >= 0) && (coordinateClick < 75)) {
+        pos.style.left = coordinateClick + 'px';   
+      } else  coordinateClick = undefined;
+
+    bar.style.width = pos.style.left;
+    overlayImg.style.width = (coordinateClick * 3.8) + 'px';
+  };
+
+// для клика по scale
+  scale.addEventListener('click', clickAndTouchOnScale);
+  scale.addEventListener ('touch', clickAndTouchOnScale);
+
+  // Для мобильного (непонятно работает ли?))
+
+
+const touchStartOnPos = function (pos) {
+  pos.addEventListener('touchstart', function(evt) {
+    evt.preventDefault();
+
+    var startCoord = {
+      x: evt.clientX
+    };
+    const touchMoveOnPos = function (evtMove){
+      // evtMove.preventDefault();
+      var shift = {
+        x: startCoord.x - evtMove.clientX
+      };
+
+      startCoord = {
+        x: evtMove.clientX
+      };
+      // Код для перемещения ползунка и бара
+      const posMover = function (positionLeft) {
+        var posPosition = null;
+      if ((positionLeft - shift.x) < 0) {
+        posPosition = 0;   
+      } else if ((positionLeft - shift.x) > 70) {
+        posPosition = 69;
+      } else {
+        posPosition = positionLeft - shift.x;
+      };
+      pos.style.left = posPosition + 'px';
+      return posPosition; 
+    };
+
+    ;
+
+    posMover(pos.offsetLeft);
+
+      // Для бара
+      
+     bar.style.width = pos.style.left;
+
+      // Для оверлея картинки
+
+      overlayImg.style.width = ((posMover(pos.offsetLeft)) * 3.8) + 'px';
+    };
+
+
+
+    // Не забудем снять обработчики
+    
+    const touchEndOnDocument = function(touchEndEvt){
+      // touchEndEvt.preventDefault();
+      document.removeEventListener('touchend', touchEndOnDocument);
+      document.removeEventListener ('touchmove', touchMoveOnPos);
+    };
+
+    document.addEventListener('touchmove', touchMoveOnPos);
+    document.addEventListener('touchend', touchEndOnDocument);
+
+  })
+};
+
+touchStartOnPos(pos);
